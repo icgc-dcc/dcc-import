2183939b5be019fbaa3b1631f2b34b53a5ef93b5
Vitalii Slobodianyk <vitalii.slobodianyk@oicr.on.ca>
2015-11-19 11:28:56 -0500

    Added install script and updated documentation

--- dcc-import-client/README.md
+++ dcc-import-client/README.md
@@ -11,3 +11,17 @@ From the command line:
 
 	mvn package
 
+Run
+---
+
+	$ dcc-import-client.sh [options]
+E.g.
+
+	$ dcc-import-client.sh --imports.sources=GENES
+	
+Update
+---
+Use the `bin/install` script to update the client library.
+For example to update to the latest release run:
+
+	$ install -l
--- dcc-import-client/src/main/bin/dcc-import-client.sh
+++ dcc-import-client/src/main/bin/dcc-import-client.sh
@@ -6,7 +6,7 @@
 #   Runs dcc-import-client
 #
 # Usage:
-#   ./dcc-import-client.sh <arguments>
+#   ./dcc-import-client.sh [arguments]
 #
 
 # Prevent this var causing issues with Spring Boot's environment binding
@@ -16,11 +16,11 @@ base_dir=$(dirname $0)/..
 java_opts="-Xmx4g"
 
 java \
-	${java_opts} \
+  ${java_opts} \
   -Dlog.dir=${base_dir}/logs \
   -Dlogging.config=${base_dir}/conf/logback.xml \
   -Dspring.config.location=file:${base_dir}/conf/ \
   -Dcom.sun.management.jmxremote.port=10001 \
   -Dcom.sun.management.jmxremote.ssl=false \
   -Dcom.sun.management.jmxremote.authenticate=false \
-	-jar ${base_dir}/lib/dcc-import-client.jar "$@"
+  -jar ${base_dir}/lib/dcc-import-client.jar "$@"
